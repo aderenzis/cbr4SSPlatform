c7b86307e9e2bc8668463c46aaea0d0268bdfb46
Cristian Mora <rapkyt@gmail.com>
2018-03-24 12:10:32 -0300

    Added one example Case.

--- src/cbr/Case.kt
+++ src/cbr/Case.kt
@@ -1,8 +1,21 @@
 package cbr
 
-import edu.giisco.SoaML.metamodel.Interface
+import edu.giisco.SoaML.metamodel.*
 
 
 data class Case(val problem: Interface, var solution: String = "") {
     val solutionIsDefined get() = solution != ""
+}
+
+fun main(args: Array<String>) {
+    val currencyType = SimpleType("string")
+    val currencyParameter = Parameter("currency", currencyType)
+    val fromCurrency = Input("fromCurrency", ArrayList(listOf(currencyParameter)))
+    val toCurrency = Output("toCurrency", ArrayList(listOf(currencyParameter)))
+    val currencyFaults = ArrayList<Fault>()
+    val currencyResponse = Response("currencyResponse", currencyType)
+    val convertCurrency = Operation("convertCurrency", fromCurrency, toCurrency, currencyFaults, currencyResponse)
+    val currencyConvertor = Interface("CurrencyConvertor", ArrayList(listOf(convertCurrency)))
+    val case1 = Case(currencyConvertor, "http://www.webservicex.net/CurrencyConvertor.asmx?WSDL")
+
 }
