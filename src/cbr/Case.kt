e4ad55da6ad7fbe2b3c4c487233dc802c1a5bc77
Cristian Mora <rapkyt@gmail.com>
2018-03-25 01:18:08 -0300

    Save an Case in Mongo
    Altough the insert can be performed, te retrieve can not be done for now.

--- src/cbr/Case.kt
+++ src/cbr/Case.kt
@@ -1,6 +1,7 @@
 package cbr
 
 import edu.giisco.SoaML.metamodel.*
+import org.litote.kmongo.*
 
 
 data class Case(val problem: Interface, var solution: String = "") {
@@ -8,6 +9,7 @@ data class Case(val problem: Interface, var solution: String = "") {
 }
 
 fun main(args: Array<String>) {
+    println("Creating Case")
     val currencyType = SimpleType("string")
     val currencyParameter = Parameter("currency", currencyType)
     val fromCurrency = Input("fromCurrency", ArrayList(listOf(currencyParameter)))
@@ -17,5 +19,18 @@ fun main(args: Array<String>) {
     val convertCurrency = Operation("convertCurrency", fromCurrency, toCurrency, currencyFaults, currencyResponse)
     val currencyConvertor = Interface("CurrencyConvertor", ArrayList(listOf(convertCurrency)))
     val case1 = Case(currencyConvertor, "http://www.webservicex.net/CurrencyConvertor.asmx?WSDL")
+    println("Case created")
+
+    println("Connecting to Mongo")
+    val client = KMongo.createClient("172.17.0.2")
+    val database = client.getDatabase("test")
+    val caseCollection = database.getCollection<Case>()
+    println("Connection with Mongo established.")
+
+    caseCollection.drop()
+    caseCollection.insertOne(case1)
+    println("Insert succefull now there are ${caseCollection.count()} Cases in the KB")
+//    val caseRetrieved = caseCollection.findOne()
+    println("Retrieved Case from db: \n $case1")
 
 }
