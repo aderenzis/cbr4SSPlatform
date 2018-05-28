15ca7c8cdf7796c6dd446d78c56fd49972049b12
Cristian Mora <rapkyt@gmail.com>
2018-05-27 23:46:20 -0300

    Move the case to the new project and add .env

--- src/cbr/CBR.kt
+++ src/cbr/CBR.kt
@@ -3,10 +3,14 @@ package cbr
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
 import edu.giisco.SoaML.metamodel.Interface
-
+import io.github.cdimascio.dotenv.dotenv
 import org.litote.kmongo.*
+import schemas.Case
 import java.time.Instant
 import java.time.format.DateTimeFormatter
+
+
+private val dotenv = dotenv { directory = "./" }
 
 
 data class RetrievedCase(val problem: Interface, val solutions: List<Pair<Double, Case>>)
@@ -23,7 +27,7 @@ fun findSimilarity(referenceCase: Case, k: Int): List<Pair<Double, Case>> {
             casesByDistance.add(Pair(distance, it))
         }
     val top = minOf(k, casesByDistance.size)
-    return casesByDistance.sortedWith(compareBy({ it.first })).slice(0 until top).map { it.second }
+    return casesByDistance.sortedWith(compareBy({ it.first })).slice(0 until top)
 }
 
 fun findSolutions(referenceCase: Case): List<Pair<Double, Case>> {
@@ -41,9 +45,12 @@ fun main(args: Array<String>) {
     val retrievedCases = mutableListOf<RetrievedCase>()
     val queryCollection = getQueryCollection()
 //TODO: insert Queries
+    val casesCOllection = getCaseCollection()
+    val example = casesCOllection.findOne()!!
+    queryCollection.insertOne(example)
     queryCollection.find().forEach {
         val solutions = findSolutions(it)
-        val retrievedCase = RetrievedCase(it, solutions)
+        val retrievedCase = RetrievedCase(it.problem, solutions)
         retrievedCases.add(retrievedCase)
     }
     val (retrievedCasesCollection, retrievedCasesCollectionName) = getRetrievedCasesCollection()
@@ -54,13 +61,16 @@ fun main(args: Array<String>) {
 
 fun getDatabase(name: String): MongoDatabase {
     println("Connecting to Mongo")
-    val client = KMongo.createClient("172.17.0.2")
+    val host = dotenv["DATABASE_HOST"] ?: "127.0.0.1"
+    val port = dotenv["DATABASE_PORT"]?.toInt() ?: 27017
+    val client = KMongo.createClient(host = host, port = port)
     return client.getDatabase(name)
 }
 
 
 fun getQueryCollection(): MongoCollection<Case> {
-    val database = getDatabase("queries")
+    val databaseName = dotenv["DATABASE_QUERIES_NAME"] ?: "queries"
+    val database = getDatabase(databaseName)
     val caseCollection = database.getCollection<Case>()
     println("Connection with Mongo established.")
     return caseCollection
@@ -68,9 +78,20 @@ fun getQueryCollection(): MongoCollection<Case> {
 
 
 fun getRetrievedCasesCollection(): Pair<MongoCollection<RetrievedCase>, String> {
-    val database_name = "retrieved_cases_${DateTimeFormatter.ISO_INSTANT.format(Instant.now())}"
+    val database_name_prefix = dotenv["DATABASE_RESULTS_PREFIX"] ?: "retrieved_cases"
+    val database_name = "${database_name_prefix}_${DateTimeFormatter.ISO_INSTANT.format(Instant.now())}"
     val database = getDatabase(database_name)
     val caseCollection = database.getCollection<RetrievedCase>()
     println("Connection with Mongo established.")
     return Pair(caseCollection, database_name)
 }
+
+
+fun getCaseCollection(): MongoCollection<Case> {
+    val databaseName = dotenv["DATABASE_KB_NAME"] ?: "KB"
+    val database = schemas.getDatabase(databaseName)
+    val caseCollection = database.getCollection<Case>()
+    println("Connection with Mongo established.")
+    return caseCollection
+}
+
